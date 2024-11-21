15948e46dd9fa58b7d7cf9d37e17226bcf03b8ff
kota3831 <171798512+kota3831@users.noreply.github.com>
2024-11-21 11:06:09 +0000

    アカウントの退会機能完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "Your accounte has been successfully cancelled."
+    redirect_to root_path
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   get 'mypage', to: 'users#index'
-  resources :users, only: [:edit, :show, :update]
+  resources :users, only: [:edit, :show, :update, :destroy]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
