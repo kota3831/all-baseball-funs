291ea39ceada24bb9eae752d12a70c71e3a8a78a
kota3831 <171798512+kota3831@users.noreply.github.com>
2024-12-25 11:50:46 +0000

    ポートフォリオ完成

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,14 +1,16 @@
 class PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    if comment.save
-      redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    if @post_comment.save
+      redirect_to post_path(@post)
     else
-      flash[:error] = comment.errors.full_messages.join(", ")
-      redirect_to post_path(post)
+      #flash[:error] = comment.errors.full_messages.join(", ")
+      #redirect_to post_path(post)
+      # @user = @post.user
+      redirect_to post_path(@post), flash: { error: @post_comment.errors.full_messages }
     end
   end
 
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,5 +2,5 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :post_comment, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :comment, presence: true, length: { minimum: 2, maximum: 50 }
 end
