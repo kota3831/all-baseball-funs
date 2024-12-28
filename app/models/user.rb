f5171bf5c09e3df926dcd2cc64a7ce6665d34ddb
kota3831 <171798512+kota3831@users.noreply.github.com>
2024-12-28 10:27:01 +0000

    コードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   end
 
   def get_profile_image(width, height)
-    unless  profile_image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   end
   
   def get_profile_image_thumbnail(width, height)
-    unless  profile_image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
